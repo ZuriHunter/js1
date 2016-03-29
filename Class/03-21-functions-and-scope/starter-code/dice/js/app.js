54166f3a5bba817544213734d4925bc39f47b9b7
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-03-28 21:19:51 -0400

    Little bit in JSON and Objects

---
+++ Class/03-21-functions-and-scope/starter-code/dice/js/app.js
@@ -0,0 +1,47 @@
+/*
+
+Creating a page where every time the user hits the "Roll Dice" button, the screen randomly updates the two dice. Use the html and css code included in the starter code folder to get started.
+
+1) Write down pseudocode for the following program.
+
+2) Follow the steps below to write your code.
+* generate a random number between 1 - 6 and store to a variable, random1
+* generate a random number between 1 - 6 and store to a variable, random2
+* combine 'dice-' and random1 to form the random class for the first die, firstDie
+* combine 'dice-' and random1 to form the random class for the second die, secondDie
+* get the first die by ID and update the class to firstDie (hint: document.getElementById)
+* get the first die by ID and update the class to secondDie (hint:document.getElementById)
+
+3) Check to see if the Dice Roll has been hit, if it has run the diceRoll function.
+
+*/
+
+
+//it is much faster to query the variables from the beginning and stuff.
+
+//firstDie = document.getElementById('first-die');
+//secondDie = document.getElementById('second-die');
+
+/* function rollDie(){
+firstDie.className = firstDie;
+secondDie.className = secondDie;
+}
+*/
+
+document.getElementById("roll-dice").addEventListener("click", rollDie);
+
+function rollDie () {
+
+   random1 = Math.floor((Math.random() * 6)+1);
+   random2 = Math.floor((Math.random() * 6)+1);
+
+  console.log(random1, random2);
+
+  firstDie = 'dice dice-' + random1;
+  secondDie = 'dice dice-'+ random2;
+
+   item1 = document.getElementById('first-die').className = firstDie;
+   item2 = document.getElementById('second-die').className= secondDie;
+
+
+}
---
+++ Class/readme.md
@@ -0,0 +1,44 @@
+# ![](assets/logo.png) JavaScript Development
+
+## People
+
+- Producer
+  - Tim Foley
+    - [tim.foley@ga.co](tim.foley@ga.co)
+    - 202-748-3694
+- Instructors
+  - Johnny Austin
+    - [johnny@isl.co](johnny@isl.co)
+    - Office hours:
+      - Thursdays 6:00pm -7:00pm [ISL Office](https://isl.co/contact/)
+      - Sundays 5:00pm - 6:00pm Remote - Hangout Link Coming!
+  - Jesse Shawl
+    - [jesse@ga.co](jesse@ga.co)
+    - office hours:
+      - Wednesdays 5:30 - 6:30
+      - Sundays 6:00pm - 7:00pm [remote](https://hangouts.google.com/hangouts/_/generalassemb.ly/ga-js-oh)
+- TA
+  - John Master
+    - [john.master@generalassemb.ly](john.master@generalassemb.ly)
+    - Office hours:
+      - Mondays 5:30 - 6:30 (General Assembly)
+      - Sundays 7:00 - 8:00 Remote - Will put appear.in link in slack
+
+## Course information
+
+Class will meet on the 8th floor in classroom 3 from 6:30 - 9:30pm - https://goo.gl/maps/PwyY2
+
+To get up to the 8th floor, show the attached Class Pass at the Security Desk downstairs. You will need to show this on your phone or printed out every time.  
+Once you're upstairs, tell the folks at the front desk you're here for your first day of Javascript and they'll point you to your classroom.
+
+### What to Bring
+
+A. Your laptop (and charger with your name on it!)
+
+B. Snacks in case you get hungry
+
+C. [Class Pass](./assets/class-pass.png) printed out or on your phone
+
+## Schedule
+
+[Google Calendar](https://calendar.google.com/calendar/embed?src=generalassemb.ly_2bpe0sg92jg3otj03epjk03foc%40group.calendar.google.com&ctz=America/New_York)
